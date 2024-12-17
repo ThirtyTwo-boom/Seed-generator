7851db49aae56c9fde841ef95d302606c5b2497c
ThirtyTwo-boom <621852549@qq.com>
2024-12-17 22:37:02 +0800

    This is a seed generator

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        Date time = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+        int seed = (int) Math.sqrt(Integer.parseInt(sdf.format(time))*30+2);
+        System.out.println(seed);
+    }
+}
